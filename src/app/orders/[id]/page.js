b32f5bd746409dd5e83759d9a4ceda1955e861c4
Vladimir <vladimirelek0@gmail.com>
2024-10-03 18:52:02 +0200

    ispravljen useEffect

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -8,16 +8,16 @@ import { useProfile } from "@/menu/useProfile";
 import toast from "react-hot-toast";
 const Order = () => {
   const [order, setOrder] = useState();
-  const [paid,setIsPaid]=useState(false);
+  const [paid, setIsPaid] = useState(false);
   const { id } = useParams();
   const { loading: profileLoading, data: admin } = useProfile();
-  const updatePayment=async()=>{
+  const updatePayment = async () => {
     const promise = new Promise(async (resolve, reject) => {
-      const res = await fetch("/api/orders",{
+      const res = await fetch("/api/orders", {
         method: "PUT",
-        body: JSON.stringify({paid,id}),
+        body: JSON.stringify({ paid, id }),
         headers: { "Content-Type": "application/json" },
-      })
+      });
       if (res.ok) {
         resolve();
       } else {
@@ -29,27 +29,25 @@ const Order = () => {
       success: "Plaćanje ažurirano",
       error: "Došlo je do greške",
     });
-      
-  }
-  if (id) {
-    useEffect(() => {
-      fetch("/api/orders?_id=" + id).then((res) =>
-        res.json().then((order) => {
-          setOrder(order);
-          setIsPaid(order.paid);
-        })
-      );
-    }, []);
-  }
-  let total=0;
-  if (order){
-    for(const product of order.cartProducts){
-      total+=cartProductPrice(product);
+  };
+
+  useEffect(() => {
+    fetch("/api/orders?_id=" + id).then((res) =>
+      res.json().then((order) => {
+        setOrder(order);
+        setIsPaid(order.paid);
+      })
+    );
+  }, []);
+
+  let total = 0;
+  if (order) {
+    for (const product of order.cartProducts) {
+      total += cartProductPrice(product);
     }
   }
   return (
     <section className="max-w-2xl mx-auto mt-8">
-    
       <div className="text-center">
         <h1 className=" text-orange-500 text-3xl font-extrabold font lg:text-4xl m-10">
           VAŠA NARUDZBA
@@ -57,38 +55,41 @@ const Order = () => {
       </div>
       {order && (
         <div className="flex justify-between gap-10">
-            <div className="bg-gray-100 p-4 rounded-lg">
-            {order.cartProducts.map(product=>(<div key={product.name}><CartProduct product={product}/></div>))} 
+          <div className="bg-gray-100 p-4 rounded-lg">
+            {order.cartProducts.map((product) => (
+              <div key={product.name}>
+                <CartProduct product={product} />
+              </div>
+            ))}
             <div className=" py-2 text-gray-500">
               <span className="text-black font-bold inline-block w-8">
-              Cijena:{total}KM
+                Cijena:{total}KM
               </span>
-              
             </div>
-            </div>
-            
+          </div>
+
           <div>
-          <AdressForm disabled={true} adressProps={order} />
-          {admin && <div>
-            <input
-              id="paid"
-              className="my-3"
-              type="checkbox"
-              checked={paid}
-              onChange={() => {
-                setIsPaid(!paid);
-              }}
-            />
-            <label className="px-2" htmlFor="paid">
-              Plaćeno
-            </label>
-            <button onClick={updatePayment}>Sačuvaj</button>
-          </div>}
+            <AdressForm disabled={true} adressProps={order} />
+            {admin && (
+              <div>
+                <input
+                  id="paid"
+                  className="my-3"
+                  type="checkbox"
+                  checked={paid}
+                  onChange={() => {
+                    setIsPaid(!paid);
+                  }}
+                />
+                <label className="px-2" htmlFor="paid">
+                  Plaćeno
+                </label>
+                <button onClick={updatePayment}>Sačuvaj</button>
+              </div>
+            )}
           </div>
         </div>
       )}
-      
-
     </section>
   );
 };
